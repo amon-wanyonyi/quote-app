a9c5ed97a3a39080583821144127b1c9accab7d7
amon-wanyonyi <amon.wanyonyi@student.moringaschool.com>
2021-12-29 20:42:58 +0300

    modified app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { QuoteComponent } from './component/quote/quote.component';
+import { QuoteFormComponent } from './component/quote-form/quote-form.component';
+import { VoteComponent } from './component/vote/vote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    QuoteComponent,
+    QuoteFormComponent,
+    VoteComponent
   ],
   imports: [
     BrowserModule,
